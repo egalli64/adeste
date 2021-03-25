935ed86906b50d775563554237661d18d37f2f58
Emanuele Galli <egalli64@gmail.com>
2021-03-25 23:24:42 +0100

    #7

--- src/main/java/adeste/ctr/Access.java
+++ src/main/java/adeste/ctr/Access.java
@@ -1,5 +1,7 @@
 package adeste.ctr;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import adeste.svc.UserSvc;
 
@@ -18,11 +21,12 @@ public class Access {
     UserSvc svc;
 
     @PostMapping("/login")
-    public String login(Model model) {
-        log.traceEntry();
+    public String login(@RequestParam String name, @RequestParam String password, Model model, HttpSession session) {
+        log.traceEntry("{}, {}", name, password);
 
-        svc.getUser("admin", "password").ifPresent(user -> {
-            model.addAttribute("user", user);   
+        svc.getUser(name, password).ifPresent(user -> {
+            session.setAttribute("user", user);
+            model.addAttribute("user", user);
         });
         return "home";
     }
