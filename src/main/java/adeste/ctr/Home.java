0b83b393c98ea89fed84a52b204168d788be6b5d
Emanuele Galli <egalli64@gmail.com>
2021-03-25 09:28:00 +0100

    #4

--- src/main/java/adeste/ctr/Home.java
+++ src/main/java/adeste/ctr/Home.java
@@ -1,7 +1,7 @@
 package adeste.ctr;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +11,14 @@ import adeste.dao.CourseRepo;
 
 @Controller
 public class Home {
-    private static Logger log = LoggerFactory.getLogger(Home.class);
+    private static Logger log = LogManager.getLogger(Home.class);
 
     @Autowired
     private CourseRepo repo;
 
     @GetMapping("/")
     public String hello(Model model) {
-        log.trace("enter hello()");
+        log.traceEntry();
         repo.findFirstByOrderByLastDesc().ifPresent(course -> model.addAttribute("course", course.getName()));
 
         return "/home";
