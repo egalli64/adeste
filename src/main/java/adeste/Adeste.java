8a84e95a2a2a0ab5295444a435888d5b36073200
Emanuele Galli <egalli64@gmail.com>
2021-09-16 07:39:06 +0200

    formatting

--- src/main/java/adeste/Adeste.java
+++ src/main/java/adeste/Adeste.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Adeste {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Adeste.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Adeste.class, args);
+    }
 }
